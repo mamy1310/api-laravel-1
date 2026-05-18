93f1573d584206c000f0074a825f8efb746d7ef0
Rakotoarisoa Andrianirina Mamitiana <mamitiana_rakotoarisoa@yahoo.com>
2026-05-18 12:55:59 +0200

    Add book api controller

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\API\BookController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -10,3 +11,5 @@ Route::get('/user', function (Request $request) {
 Route::get('/ping', function () {
     return response()->json(['message' => 'pong']);
 });
+
+Route::apiResource('books', BookController::class);
