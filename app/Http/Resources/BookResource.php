ba1941e63fbd3c1bab4123b5499217506383c783
Rakotoarisoa Andrianirina Mamitiana <mamitiana_rakotoarisoa@yahoo.com>
2026-05-19 10:55:38 +0200

    HATEOAS

--- app/Http/Resources/BookResource.php
+++ app/Http/Resources/BookResource.php
@@ -14,6 +14,12 @@ class BookResource extends JsonResource
             'author'  => strtoupper($this->author),
             'summary' => $this->summary,
             'isbn'    => $this->isbn,
+            '_links'  => [
+                'self'   => route('books.show', ['book' => $this->id]),
+                'update' => route('books.update', ['book' => $this->id]),
+                'delete' => route('books.destroy', ['book' => $this->id]),
+                'all'    => route('books.index'),
+            ],
         ];
     }
 }
